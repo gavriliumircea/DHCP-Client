1e20a204f3feac90912022cc57f1bd8b19aa437b
gavriliumircea <48255669+gavriliumircea@users.noreply.github.com>
2019-11-26 18:27:04 +0200

    comunicare prin intermediul tipului de mesaj

--- Proiect_retele/venv/Scripts/main.py
+++ Proiect_retele/venv/Scripts/main.py
@@ -1,18 +1,13 @@
 import socket;
-import json
 
-Messages_dictionary={
-    "DHCPDISCOVER":1,
-    "DHCPOFFER":2,
-    "DHCPREQUEST":3,
-    "DHCPDECLINE":4,
-    "DHCPACK":5,
-    "DHCPNAK":6,
-    "DHCPRELEASE":7,
-    "DHCPINFORM":8
-}
+message_size=int(225)
 
 Client_States_Dictionary={1:"initial",2:"waiting for DHCPOFFER",3:"sending DHCP request", 4:"Waiting for DHCPACK", 5:"Complete", 6:"Realese IP address",7:"closed"}
+
+class DHCP_Client:
+    STATE=1;
+    IP_ADDRESS="127.0.0.1"
+    UDP_PORT=68
 
 class Logger:
     def __init__(self):
@@ -28,16 +23,41 @@ class Logger:
     def endCommunication(self):
         self.f.close()
 
-class DHCP_Client:
-    STATE=1;
-    IP_ADDRESS="127.0.0.1"
-    UDP_PORT=68
+Messages_dictionary={
+    "DHCPDISCOVER":1,
+    "DHCPOFFER":2,
+    "DHCPREQUEST":3,
+    "DHCPDECLINE":4,
+    "DHCPACK":5,
+    "DHCPNAK":6,
+    "DHCPRELEASE":7,
+    "DHCPINFORM":8
+}
+
+def message_factory(type):
+    message=bytearray(message_size)
+    message[0]=1;
+    message[1]=1;
+    message[2]=6;
+    message[224]=type;
+    return message
+
+def check_message(message, state):
+    if message[0]==2:
+        return True
+    elif message[224]==state:
+        return True
+    return False
+
+
+
+
+
+
 
 logger=Logger()
-
 UDP_PORT_TO_TRANSMIT = 67;
 MESSAGE = "type: 1"
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 message_from_server=0
@@ -49,30 +69,41 @@ while(Client_States_Dictionary[client.STATE]!="closed"):
     if client.STATE==1:
             logger.writeInfo("am starea "+str(client.STATE))
             print("am trimis")
-            sock.sendto(json.dumps(MESSAGE).encode(), ('<broadcast>', 67))
-            logger.writeInfo("am trimis mesajul "+MESSAGE)
+            sock.sendto(message_factory(client.STATE), ('<broadcast>', 67))
+            logger.writeInfo("am trimis mesajul de tipul "+str(message_factory(client.STATE)[224]))
             client.STATE=2
     elif client.STATE==2:
         print("asteptam")
-        msg=sock.recvfrom(1024)
-        print(msg[0])
-        logger.writeInfo("am starea " +str(client.STATE))
-        client.STATE=3
+        byteArray=sock.recvfrom(1024)
+        msg=byteArray[0]
+        print(msg[224])
+        if check_message(msg,client.STATE)==True:
+            print("am primit mesajul de tipul "+str(msg[224]))
+#           logger.writeInfo("am starea " +str(client.STATE))
+            client.STATE=3
     elif client.STATE == 3:
+        print("sunt in starea "+str(client.STATE))
         logger.writeInfo("am starea "+str(client.STATE))
+        sock.sendto(message_factory(client.STATE), ('<broadcast>', 67))
         client.STATE=4
     elif client.STATE == 4:
-        logger.writeInfo("am starea "+str(client.STATE))
-        client.STATE = 5
+        #logger.writeInfo("am starea "+str(client.STATE))
+        print("stare "+ str(client.STATE))
+        bytearray=sock.recvfrom(1024)
+        msg=bytearray[0]
+        print(msg[224])
+        if check_message(msg,client.STATE)==True:
+            print("am primit mesajul de tipul " + str(msg[224]))
+           # client.STATE = 5
     elif client.STATE == 5:
         logger.writeInfo("am starea " + str(client.STATE))
-        client.STATE = 6
+        #client.STATE = 6
     elif client.STATE == 6:
         logger.writeInfo("am starea " +str(client.STATE))
-        client.STATE = 7
+        #client.STATE = 7
     elif client.STATE == 7:
         logger.writeInfo("am starea "+str(client.STATE))
-        client.STATE = 8
+        #client.STATE = 8
     logger.writeInfo("Comunicatie terminata")
 print("am iesit din while")
 logger.endCommunication()
--- server_test/venv/Scripts/main.py
+++ server_test/venv/Scripts/main.py
@@ -1,19 +1,32 @@
 import socket;
 import json;
+
+message_size=225
+
+def message_factory(type):
+    message=bytearray(message_size)
+    message[0]=2;
+    message[1]=1;
+    message[2]=6;
+    message[224]=type;
+    return message
+
 UDP_IP="127.0.0.2";
 UDP_PORT = 67;
 sock2= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bufferSize=1024;
 sock2.bind((UDP_IP, UDP_PORT))
 mesaj="server"
-bytesAddressPair=0;
+bytesAddressPair=0
 
 while True:
     bytesAddressPair = sock2.recvfrom(bufferSize)
     if(bytesAddressPair!=0):
         message = bytesAddressPair[0]
         address = bytesAddressPair[1]
-        if(message.decode().__contains__("type: 1")):
-            print("Am primit mesajul: ", message.decode())
+        if(message[0]==1):
+            rcv_msg_type=message[224]
+            print("Am primit mesajul: ", message[224])
             print("de la adresa: ", address)
-            sock2.sendto(mesaj.encode(),address)
+            print(message_factory(rcv_msg_type + 1)[224])
+            sock2.sendto(message_factory(rcv_msg_type+1),address)
